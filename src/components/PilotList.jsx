4bb53580c3793312f6d740f7237990a6e51931b3
Daria Zharkova <dariazharkova1991@gmail.com>
2025-03-06 18:06:01 +0200

    module1 lesson1

---
+++ src/components/PilotList.jsx
@@ -0,0 +1,13 @@
+import PilotProfile from './PilotProfile';
+
+export default function PilotList({ items }) {
+  return (
+    <ul>
+      {items.map(officer => (
+        <li key={officer.id}>
+          <PilotProfile pilot={officer} />
+        </li>
+      ))}
+    </ul>
+  );
+}
